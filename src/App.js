d480acd082e46c912107e083908abe44ee6ce7fa
harshitchopra7 <chopraharshit2000@gmail.com>
2022-06-04 13:21:24 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="App">hello</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JZIaqb4qHsb0YHkwO9Nv5D07iEMZ2PiHTGwz3Us&s" />
     </div>
   );
 }
